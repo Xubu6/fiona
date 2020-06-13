d61d1e44409532087e6ae8e78c9cd8ba3a6c7c9f
Xubu6 <a.x.alxu@gmail.com>
2020-06-12 22:51:28 -0400

    First Deploy

--- server/index.js
+++ server/index.js
@@ -7,6 +7,19 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const favicon = require('express-favicon');
+const path = require('path');
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 const client = require("twilio")(
   process.env.TWILIO_ACCOUNT_SID,
   process.env.TWILIO_AUTH_TOKEN
